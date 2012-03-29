7af78f4aa9c47d50a107b60bc39ae350aaba2f8a
Vicent Gozalbes <vigosan@gmail.com>
2012-03-29 23:54:01 +0200

    Extract source ID from resource

--- lib/big_ml/source.rb
+++ lib/big_ml/source.rb
@@ -16,5 +16,8 @@ module BigML
         }
      end
 
+     def id
+       resource.split('/').last
+     end
   end
 end
--- spec/big_ml/source_spec.rb
+++ spec/big_ml/source_spec.rb
@@ -11,5 +11,10 @@ describe BigML::Source do
       source = BigML::Source.new('code' => nil)
       source.code.should be_nil
     end
+
+    it "extract source id from resource" do
+      source = BigML::Source.new('resource' => 'souce/4f66a0b903ce8940c5000000')
+      source.id.should == "4f66a0b903ce8940c5000000"
+    end
    end
 end

50831e9f0624fa02960f96af8e85734123929bf6
Vicent Gozalbes <vigosan@gmail.com>
2012-03-30 22:17:38 +0200

    Add put and fix post body

--- lib/big_ml/request.rb
+++ lib/big_ml/request.rb
@@ -4,8 +4,12 @@ module BigML
       self.class.get(path, :query => credentials.merge(options))
     end
 
+    def put(path, options = {})
+      self.class.put(path, :body => credentials.merge(options))
+    end
+
     def post(path, options = {})
-      self.class.post(path, :query => credentials.merge(options))
+      self.class.post(path, :body => credentials.merge(options))
     end
 
     def delete(path)

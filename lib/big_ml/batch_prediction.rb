a9b5ef57a51a05f969fa099e5bf143978968f0cf
Troels Knak-Nielsen <troelskn@gmail.com>
2014-03-07 09:33:18 +0100

    Removed status checking code

--- lib/big_ml/batch_prediction.rb
+++ lib/big_ml/batch_prediction.rb
@@ -1,5 +1,4 @@
 require 'big_ml/base'
-require 'timeout'
 
 module BigML
   class BatchPrediction < Base
@@ -28,28 +27,5 @@ module BigML
       self.class.download(id)
     end
 
-    def ready?
-      success? || error?
-    end
-
-    def success?
-      status["code"] == 5
-    end
-
-    def error?
-      status["code"] < 0
-    end
-
-    def self.wait_for_ready(resource_or_id, options = {:delay => 1, :timeout => 60})
-      resource = nil
-      remaining = options[:timeout]
-      until resource && resource.ready?
-        resource = self.find(resource_or_id.kind_of?(BigML::Base) ? resource_or_id.id : resource_or_id)
-        sleep(options[:delay]) unless resource.ready?
-        remaining = remaining - options[:delay]
-        raise Timeout::Error if remaining <= 0
-      end
-      resource
-    end
   end
 end
